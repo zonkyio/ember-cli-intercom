a628689599def574a3e5f685aaefe42861a1f629
Jan Bobisud <me@bobisjan.com>
2016-09-11 11:31:27 +0200

    Add support for CSP Level 1

--- addon/services/intercom.js
+++ addon/services/intercom.js
@@ -8,6 +8,7 @@ export default Ember.Service.extend({
 
   boot(params = {}) {
     if (this.get('fastboot.isFastBoot')) { return; }
+    if (!window.Intercom) { return; }
     let config = getOwner(this)._lookupFactory('config:environment');
     params['app_id'] = config['ember-cli-intercom'].appId;
     window.Intercom('boot', params);
@@ -15,31 +16,37 @@ export default Ember.Service.extend({
 
   update(params) {
     if (this.get('fastboot.isFastBoot')) { return; }
+    if (!window.Intercom) { return; }
     window.Intercom('update', params);
   },
 
   shutdown() {
     if (this.get('fastboot.isFastBoot')) { return; }
+    if (!window.Intercom) { return; }
     window.Intercom('shutdown');
   },
 
   hide() {
     if (this.get('fastboot.isFastBoot')) { return; }
+    if (!window.Intercom) { return; }
     window.Intercom('hide');
   },
 
   show() {
     if (this.get('fastboot.isFastBoot')) { return; }
+    if (!window.Intercom) { return; }
     window.Intercom('show');
   },
 
   showMessages() {
     if (this.get('fastboot.isFastBoot')) { return; }
+    if (!window.Intercom) { return; }
     window.Intercom('showMessages');
   },
 
   showNewMessage(text) {
     if (this.get('fastboot.isFastBoot')) { return; }
+    if (!window.Intercom) { return; }
 
     if (text) {
       window.Intercom('showNewMessage', text);
@@ -50,6 +57,7 @@ export default Ember.Service.extend({
 
   trackEvent(eventName, params) {
     if (this.get('fastboot.isFastBoot')) { return; }
+    if (!window.Intercom) { return; }
     window.Intercom('trackEvent', eventName, params);
   }
 });
